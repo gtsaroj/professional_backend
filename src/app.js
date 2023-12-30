a707cd51f51cb4eb2a09690d8b20867c3d3800da
Saroj <sarojgt326@gmail.com>
2023-12-30 16:00:55 +0545

    API handler

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import express from "express"
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_URI,
+    Credential: true,
+}))
+
+app.use(express.json({limit: '16kb'}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
